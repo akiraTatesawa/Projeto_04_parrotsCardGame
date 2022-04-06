42ab4d5f551158cf2475778a1326e59e59f5298e
Arthur Akira <arthurak10.at@gmail.com>
2022-04-06 14:54:00 -0300

    adiciona cartas embaralhadas s/animação

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
-const imagens = ["bobrossparrot.gif", "explodyparrot.gif", "fiestaparrot.gif", "metalparrot.gif", "revertitparrot.gif", "tripletsparrot.gif", "unicornparrot.gif"]
+const imagens = ["bobrossparrot.gif", "explodyparrot.gif", "fiestaparrot.gif", "metalparrot.gif", "revertitparrot.gif", "tripletsparrot.gif", "unicornparrot.gif"];
+let cartas;
 
 function comparador() { 
 	return Math.random() - 0.5; 
@@ -21,13 +22,24 @@ function pedeNumCartas() {
 //embaralhar as imagens
 //dividir o numCartas por 2
 //adicionar uma carta * 2 numa lista 
-//embaralhar a lista
+//embaralhar as cartas
 //colocar as cartas no DOM
 
 function iniciaJogo() {
-    let numeroCartas = pedeNumCartas();
+    const cartasArr = [];
+    const numeroCartas = pedeNumCartas();
     imagens.sort(comparador);
     
-
-
+    for (i = 0; i < numeroCartas/2; i++) {
+        cartasArr.push(`<div class='carta'>\n  <img src='media/${imagens[i]}' alt='GIF de um papagaio animado'/>\n</div>`);
+        cartasArr.push(`<div class='carta'>\n  <img src='media/${imagens[i]}' alt='GIF de um papagaio animado'/>\n</div>`);
+    }
+    cartasArr.sort(comparador);
+    
+    cartas = document.querySelector(".cartas");
+    for (j = 0; j < cartasArr.length; j++) {
+        cartas.innerHTML += cartasArr[j];
+    }
 }
+
+iniciaJogo()
