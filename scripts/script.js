1b7462ecb8379b1a99eb3b3488cb8eb9836aed68
Arthur Akira <arthurak10.at@gmail.com>
2022-04-08 10:43:13 -0300

    fim de jogo implementado

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,29 @@
-const parrots = ["bobrossparrot", "explodyparrot", "fiestaparrot", "metalparrot", "revertitparrot", "tripletsparrot", "unicornparrot"];
+const parrots = [
+    "./media/bobrossparrot.gif", 
+    "./media/explodyparrot.gif", 
+    "./media/fiestaparrot.gif", 
+    "./media/metalparrot.gif", 
+    "./media/revertitparrot.gif", 
+    "./media/tripletsparrot.gif", 
+    "./media/unicornparrot.gif"];
+
 const cartasArr = [];
+let listaGira = [];
+let armazenaCodigoCartasViradas = [];
+let armazenaCartasViradas = [];
+let contaAcertos = 0;
+let contaJogadas = 0;
 
 // [{nome: "bobrossparrot", status: "frente"}, {nome: "explodyparrot", status: "frente"}, ...]
 
 
 function comparador () { 
+
 	return Math.random() - 0.5; 
 }
 
 function pedeNumCartas () {
+
     let numCartas;
     let numValido = false;
 
@@ -22,9 +37,10 @@ function pedeNumCartas () {
 }
 
 function distribuiCartas (numeroCartas) {
+
     for (let i = 0; i < numeroCartas/2; i++) {
-        cartasArr.push({nome: parrots[i], status: "frente", par: "1"})
-        cartasArr.push({nome: parrots[i], status: "frente", par: "2"})
+        cartasArr.push({nome: parrots[i], status: "frente"})
+        cartasArr.push({nome: parrots[i], status: "frente"})
     }
     cartasArr.sort(comparador);
     console.log(cartasArr); 
@@ -37,7 +53,7 @@ function distribuiCartas (numeroCartas) {
                 <img src='./media/front.png' alt='Ilustração de um papagaio'/>
             </div>
             <div class="carta traseira">
-                <img src='./media/${cartasArr[i].nome}.gif' alt='GIF de um papagaio animado'/>
+                <img src='${cartasArr[i].nome}' alt='GIF de um papagaio animado'/>
             </div>
         </div>`
     }
@@ -50,34 +66,66 @@ function iniciaJogo () {
     distribuiCartas(numeroCartas);
 }
 
-function giraCarta (el) {
-    el.classList.add("gira");
+function removeOnclick () {
+    armazenaCodigoCartasViradas[0].removeAttribute("onclick");
+    armazenaCodigoCartasViradas[1].removeAttribute("onclick"); 
+}
+
+function adicionaOnclick () {
+    armazenaCodigoCartasViradas[0].setAttribute("onclick", "giraCarta(this)");
+    armazenaCodigoCartasViradas[1].setAttribute("onclick", "giraCarta(this)");
 }
 
 
-iniciaJogo()
+function removeGiraDuasCartas() {
+    armazenaCodigoCartasViradas[0].classList.remove("gira");
+    armazenaCodigoCartasViradas[1].classList.remove("gira");
+    adicionaOnclick();
+    armazenaCodigoCartasViradas = [];
+}
 
+function fim () {
+    alert(`Você ganhou em ${contaJogadas} jogadas!`);
+}
 
+function verificaFim (numeroCartas) {
+    if (contaAcertos === numeroCartas/2) {
+        setTimeout(fim, 1000);
+    }
 
-// for (let i = 0; i < numeroCartas/2; i++) {
-//     cartasArr.push(`
-//     <div class='carta_container' onclick="giraCarta(this)">
-//         <div class="carta frontal">
-//             <img src='./media/front.png' alt='Ilustração de um papagaio'/>
-//         </div>
-//         <div class="carta traseira">
-//             <img src='./media/${cartasArr[i].nome}.gif' alt='GIF de um papagaio animado'/>
-//         </div>
-//     </div>
-//     `);
-//     cartasArr.push(`
-//     <div class='carta_container' onclick="giraCarta(this)">
-//         <div class="carta frontal">
-//             <img src='./media/front.png' alt='Ilustração de um papagaio'/>
-//         </div>
-//         <div class="carta traseira">
-//             <img src='./media/${imagens[i]}' alt='GIF de um papagaio animado'/>
-//         </div>
-//     </div>
-//     `);
-// }
+}
+
+function giraCarta (el) {
+
+    const cartaVirada = el.querySelector(".traseira img").getAttribute("src");
+    el.classList.add("gira");
+    armazenaCodigoCartasViradas.push(el);
+    armazenaCartasViradas.push(cartaVirada);
+    console.log(armazenaCartasViradas);
+    console.log(armazenaCodigoCartasViradas);
+
+    for (let i = 0; i < armazenaCartasViradas.length; i++) {
+        armazenaCodigoCartasViradas[i].removeAttribute("onclick");
+    }
+
+    if (armazenaCartasViradas.length === 2) {
+        if (armazenaCartasViradas[0] === armazenaCartasViradas[1]) {
+            console.log("é igual");
+            removeOnclick();
+            armazenaCartasViradas = [];
+            armazenaCodigoCartasViradas = [];
+            contaAcertos++;
+            console.log(contaAcertos);
+        } else {
+            console.log("é diferente");
+            setTimeout(removeGiraDuasCartas, 2000);
+            armazenaCartasViradas = [];
+        }
+    }
+
+    contaJogadas++;
+    console.log(`Jogada número ${contaJogadas}`);
+    verificaFim(cartasArr.length);
+}
+
+iniciaJogo();
