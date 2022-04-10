9ceba499d48574ede3a81b602379fade82103f0f
Arthur Akira <arthurak10.at@gmail.com>
2022-04-10 17:13:14 -0300

    Bônus: recomeçar jogo

--- scripts/script.js
+++ scripts/script.js
@@ -7,28 +7,24 @@ const parrots = [
     "./media/tripletsparrot.gif", 
     "./media/unicornparrot.gif"];
 
-const cartasArr = [];
-let listaGira = [];
+let cartasArr = [];
 let armazenaCodigoCartasViradas = [];
 let armazenaCartasViradas = [];
 let contaAcertos = 0;
 let contaJogadas = 0;
 
-// [{nome: "bobrossparrot", status: "frente"}, {nome: "explodyparrot", status: "frente"}, ...]
-
 
 function comparador () { 
-
 	return Math.random() - 0.5; 
 }
 
 function pedeNumCartas () {
-
+//  Função pede um número de cartas e só retorna o valor quando o número for par e estiver entre 4 e 14.
     let numCartas;
     let numValido = false;
 
     do {
-        numCartas = prompt("Digite o número de cartas\nInsira um número entre 4 e 14:");
+        numCartas = prompt("Digite o número de cartas\n[Insira um número par entre 4 e 14]");
         if (numCartas%2 === 0 && (numCartas >= 4 && numCartas <= 14)) {
             numValido = true;
         }
@@ -37,15 +33,19 @@ function pedeNumCartas () {
 }
 
 function distribuiCartas (numeroCartas) {
-
+//  Cria duas cartas idênticas e adiciona à lista cartasArr
     for (let i = 0; i < numeroCartas/2; i++) {
-        cartasArr.push({nome: parrots[i], status: "frente"})
-        cartasArr.push({nome: parrots[i], status: "frente"})
+        cartasArr.push({nome: parrots[i]})
+        cartasArr.push({nome: parrots[i]})
     }
+
+// Embaralha a lista de cartas
     cartasArr.sort(comparador);
     console.log(cartasArr); 
 
+//  Adiciona as cartas embaralhadas ao DOM
     let cartas = document.querySelector(".cartas");
+    cartas.innerHTML = "";
     for (let i = 0; i < cartasArr.length; i++) {
         cartas.innerHTML += 
         `<div class='carta_container' onclick="giraCarta(this)">
@@ -59,25 +59,37 @@ function distribuiCartas (numeroCartas) {
     }
 }
 
-
 function iniciaJogo () {
+//  Recebe um número de cartas e realiza o embaralhamento e sorteio
     const numeroCartas = pedeNumCartas();
     parrots.sort(comparador);
     distribuiCartas(numeroCartas);
+    //iniciaTimer();
 }
 
+// function iniciaTimer () {
+//     const timer = document.querySelector(".timer");
+//     let segundos = 0;
+//     const timerInterval = setInterval(function () {
+//         segundos ++;
+//         timer.innerHTML = `${segundos}`;
+//     }, 1000);
+// }
+
 function removeOnclick () {
+//  Remove permanentemente o onclick das cartas que já foram pontuadas
     armazenaCodigoCartasViradas[0].removeAttribute("onclick");
     armazenaCodigoCartasViradas[1].removeAttribute("onclick"); 
 }
 
 function adicionaOnclick () {
+//  Adiciona o onclick nas duas cartas que foram viradas, porém não eram iguais
     armazenaCodigoCartasViradas[0].setAttribute("onclick", "giraCarta(this)");
     armazenaCodigoCartasViradas[1].setAttribute("onclick", "giraCarta(this)");
 }
 
-
 function removeGiraDuasCartas() {
+//  Remove a classe de transição das cartas que foram viradas, porém não eram iguais
     armazenaCodigoCartasViradas[0].classList.remove("gira");
     armazenaCodigoCartasViradas[1].classList.remove("gira");
     adicionaOnclick();
@@ -86,39 +98,49 @@ function removeGiraDuasCartas() {
 
 function fim () {
     alert(`Você ganhou em ${contaJogadas} jogadas!`);
+    let pergunta;
+    do {
+        pergunta = prompt("Deseja jogar novamente?\n[sim/não]")
+    } while (pergunta !== "sim" && pergunta !== "não");
+
+    if (pergunta === "sim") {
+        cartasArr = [];
+        armazenaCodigoCartasViradas = [];
+        armazenaCartasViradas = [];
+        contaAcertos = 0;
+        contaJogadas = 0;
+        iniciaJogo();
+    }
 }
 
 function verificaFim (numeroCartas) {
+//  Confere se o número de acertos é igual à quantidade de pares de cartas
     if (contaAcertos === numeroCartas/2) {
         setTimeout(fim, 1000);
     }
-
 }
 
 function giraCarta (el) {
-
+//  Armazena a identidade da carta virada
     const cartaVirada = el.querySelector(".traseira img").getAttribute("src");
     el.classList.add("gira");
     armazenaCodigoCartasViradas.push(el);
     armazenaCartasViradas.push(cartaVirada);
-    console.log(armazenaCartasViradas);
-    console.log(armazenaCodigoCartasViradas);
-
+    
+//  Remove onclick da carta que acaba de ser virada
     for (let i = 0; i < armazenaCartasViradas.length; i++) {
         armazenaCodigoCartasViradas[i].removeAttribute("onclick");
     }
 
+//  Confere se as cartas viradas são iguais ou não e retira o clique do site enquanto isso
     if (armazenaCartasViradas.length === 2) {
         document.querySelector(".conteudo").classList.add("sem_clique");
         if (armazenaCartasViradas[0] === armazenaCartasViradas[1]) {
-            console.log("é igual");
             removeOnclick();
             armazenaCartasViradas = [];
             armazenaCodigoCartasViradas = [];
             contaAcertos++;
-            console.log(contaAcertos);
         } else {
-            console.log("é diferente");
             setTimeout(removeGiraDuasCartas, 1500);
             armazenaCartasViradas = [];
         }
@@ -127,8 +149,8 @@ function giraCarta (el) {
         }, 1500);
     }
 
+//  Acrescenta o número de jogadas e verifica se o jogo acabou
     contaJogadas++;
-    console.log(`Jogada número ${contaJogadas}`);
     verificaFim(cartasArr.length);
 }
 
